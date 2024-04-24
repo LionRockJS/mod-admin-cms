01baad3b1f3583850cf8286e1f3cddf52a06554a
Colin Leung <colin@cowise.co>
2024-04-25 04:39:14 +0800

    fix: index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import url from "node:url";
 const dirname = url.fileURLToPath(new URL('.', import.meta.url)).replace(/\/$/, '');
 export default {dirname}
 
-import ControllerAdminPage from "./classes/controller/admin/Page";
-import ControllerAdminContent from "./classes/controller/admin/Content";
-import ControllerAdminTag from "./classes/controller/admin/Tag";
-import HelperPageText from "./classes/helper/PageText";
+import ControllerAdminPage from "./classes/controller/admin/Page.mjs";
+import ControllerAdminContent from "./classes/controller/admin/Content.mjs";
+import ControllerAdminTag from "./classes/controller/admin/Tag.mjs";
+import HelperPageText from "./classes/helper/PageText.mjs";
 
 export {
   ControllerAdminPage,
