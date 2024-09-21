de06289e74fd4268d19c36f9b373ce0591c0360e
Colin Leung <colin@cowise.co>
2024-09-22 03:29:36 +0800

    chore: export Models

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ import ControllerAdminPage from "./classes/controller/admin/Page.mjs";
 import ControllerAdminContent from "./classes/controller/admin/Content.mjs";
 import ControllerAdminTag from "./classes/controller/admin/Tag.mjs";
 import HelperPageText from "./classes/helper/PageText.mjs";
+import ModelPage from "./classes/model/Page.mjs";
+import ModelTag from "./classes/model/Tag.mjs";
+import ModelTagType from "./classes/model/TagType.mjs";
 
 export {
   ControllerAdminPage,
   ControllerAdminContent,
   ControllerAdminTag,
   HelperPageText,
+  ModelPage,
+  ModelTag,
+  ModelTagType,
 };
